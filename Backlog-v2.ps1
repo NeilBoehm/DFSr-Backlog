e2f9e848c8a00090c704b98418c9ecfba11168f7
NeilBoehm <neilboehm@hotmail.com>
2016-04-12 08:29:51 -0700

    Added files via upload

---
+++ Backlog-v2.ps1
@@ -0,0 +1,115 @@
+﻿$File_Path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+If (-Not(Test-Path "$File_Path\Logs")){New-Item "$File_Path\Logs" -ItemType Directory}
+$LogFile = "$File_Path\Logs\$(Get-Date -format yyyy-MM-dd)-Backlog.csv"
+Get-ChildItem -Path "$File_Path\Logs" | Where-Object {$_.PSisContainer -eq $false -and $_.LastWriteTime -lt (Get-date).AddDays(-183)} | Remove-Item -Force
+[int]$BacklogThreshold = '75'
+#-----------------------------------------------------------------------
+$To = 'Email@SomeWhere.com'
+$From = 'Email@SomeWhere.com'
+$Subject = "DFSr Backlog Counts - $((get-date).ToString('MM/dd/yyyy'))"
+$MailServer = 'mail.SomeWhere.com'
+#-----------------------------------------------------------------------
+#-----------------------------------------------------------------------
+$Style = '<style>'
+$Style = $Style + 'TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}'
+$Style = $Style + 'TH{border-width: 1px;padding: 5px;border-style: solid;border-color: black;background-color:DarkGray;text-align:center}'
+$Style = $Style + 'TD{border-width: 1px;padding: 5px;border-style: solid;border-color: black;text-align:center}'
+$Style = $Style + '</style>'
+#-----------------------------------------------------------------------
+$elapsed = [System.Diagnostics.Stopwatch]::StartNew()
+$JobCount = 0
+$Domains = 'Domain1','Domain2'
+$Output = @()
+$EmailBody = @()
+$ProcessedJobCount = 0
+Function Get-RepGroups {Param($Domain)
+    $RepGroupCommand = "Dfsradmin rg list /Domain:'" + $Domain + "' /attr:rgname /csv"
+    Invoke-Expression -Command $RepGroupCommand | Where-Object {-not($_ -like 'RgName' -or $_ -like '' -or $_ -like 'Domain System Volume')}
+    #Invoke-Expression -Command $RepGroupCommand | Where-Object {$_ -like 'A*'}
+}
+Function Get-Members {Param($Domain,$RepGroupName)
+    $MembersCommand = "DfsrAdmin Conn list /Domain:'" + $Domain + "' /RgName:'" + $RepGroupName + "' '/attr:SendMem,RecvMem' /csv"
+    Invoke-Expression -Command $MembersCommand | Where-Object {-not($_ -like 'Sendmem*RecvMem' -or $_ -like '')}
+}
+Function Get-Folders {Param($Domain,$RepGroupName)
+    $FolderListCommand = "DfsrAdmin RF List /Domain:'" + $Domain + "' /RgName:'" + $RepGroupName + "' /attr:RfName /csv"
+    Invoke-Expression -Command $FolderListCommand | Where-Object {-not($_ -like 'RfName' -or $_ -like '')}
+}
+Function Que-Job {Param($Member,$RepGroupName,$ReplicatedFolder,$RetryCount=0)
+    [string]$BacklogCommand = "DfsrDiag Backlog /SendingMember:'" + $($Member.Split(',')[0]) + "' /ReceivingMember:'" + $($Member.Split(',')[1]) + "' /RgName:'" + $RepGroupName + "' /RFName:'" + $ReplicatedFolder + "'"
+    $Global:JobCount++
+    $Global:JobsCreated += "$Domain#$RepGroupName#$ReplicatedFolder#$($Member.Split(',')[0])#$($Member.Split(',')[1])#$($RetryCount)"
+    while (@(Get-Job -State Running).Count -ge 100) {Start-Sleep -Seconds 2}
+    Start-Job -Name "$Domain#$RepGroupName#$ReplicatedFolder#$($Member.Split(',')[0])#$($Member.Split(',')[1])#$($RetryCount)" -ScriptBlock {param($BacklogCommand)
+        Invoke-Expression -Command $BacklogCommand
+    } -ArgumentList $BacklogCommand | Out-Null
+}
+Function Gather-Jobs{
+    Get-Job | Foreach {
+    $GatherWorkingJob = $_
+    $JobsProcessed += $GatherWorkingJob.Name
+    $GatherReceivedJob = Receive-Job $GatherWorkingJob.Id -Keep | Where-Object {$_ -like '*Backlog File Count:*' -or $_ -like '*Operation Failed*' -or $_ -like ''}
+    If($GatherReceivedJob -like '*Operation Failed*'){
+        If($([int]($GatherWorkingJob.name).Split('#')[-1]) -ne 10){
+            $Retry = [int](($GatherWorkingJob.name).Split('#')[-1])+1
+            $Global:ReRanJobCount++
+            Que-Job -Member "$(($GatherWorkingJob.name).Split('#')[3]),$(($GatherWorkingJob.name).Split('#')[4])" -RepGroupName $(($GatherWorkingJob.name).Split('#')[1]) -ReplicatedFolder $(($GatherWorkingJob.name).Split('#')[2]) -RetryCount $Retry
+            Remove-Job $GatherWorkingJob.Id -ErrorAction SilentlyContinue
+        }
+    }
+}
+}
+Function Process-Jobs {
+    Get-Job | Foreach {
+        $ProcessWorkingJob = $_
+        $Global:JobsProcessed += $ProcessWorkingJob.Name
+        $ProcessReceivedJob = Receive-Job $ProcessWorkingJob.Id | Where-Object {$_ -like '*Backlog File Count:*' -or $_ -like '*Operation Failed*'}
+        $Backlog = $Null
+        $Global:ProcessedJobCount++
+        If($ProcessReceivedJob -like '*Operation Failed*'){$ProcessBacklog = '**ERROR**'}
+        Else{$ProcessBacklog = If($ProcessReceivedJob -ne $Null){$($ProcessReceivedJob -split ' {1,}')[-1]}Else{'0'}}
+        If($ProcessBacklog -eq '**ERROR**' -or [int]$ProcessBacklog -ge $BacklogThreshold){$CommandRan = "DfsrDiag Backlog /SendingMember:'" + $(($ProcessWorkingJob.name).Split('#')[3]) + "' /ReceivingMember:'" + $(($ProcessWorkingJob.name).Split('#')[4]) + "' /RgName:'" + $(($ProcessWorkingJob.name).Split('#')[1]) + "' /RFName:'" + $(($ProcessWorkingJob.name).Split('#')[2]) + "'"}Else{$CommandRan = ''}
+            $Global:Output += [PSCustomObject] @{
+	        'Domain' = $((($ProcessWorkingJob.name).Split('#')).Split('.')[0])
+	        'Rep Group' = $(($ProcessWorkingJob.name).Split('#')[1])
+	        'Replicated Folder' = $(($ProcessWorkingJob.name).Split('#')[2])
+            'Sending Member' = $(($ProcessWorkingJob.name).Split('#')[3])
+            'Receiving Member' = $(($ProcessWorkingJob.name).Split('#')[4])
+            'BackLog' = $ProcessBacklog
+            'Command' = $CommandRan
+        }
+        If(($ProcessReceivedJob -ne $Null) -and ($ProcessBacklog -eq '**ERROR**' -or [int]$ProcessBacklog -ge $BacklogThreshold)){
+            $Global:EmailBody += [PSCustomObject] @{
+	        'Domain' = $((($ProcessWorkingJob.name).Split('#')).Split('.')[0])
+	        'Rep Group' = $(($ProcessWorkingJob.name).Split('#')[1])
+	        'Replicated Folder' = $(($ProcessWorkingJob.name).Split('#')[2])
+            'Sending Member' = $(($ProcessWorkingJob.name).Split('#')[3])
+            'Receiving Member' = $(($ProcessWorkingJob.name).Split('#')[4])
+            'BackLog' = $ProcessBacklog
+            }
+        }
+    }
+    Remove-Job $ProcessWorkingJob.Id -ErrorAction SilentlyContinue
+}
+
+Foreach ($Domain in $Domains) {
+Get-RepGroups -Domain $Domain | Foreach {
+    $RepGroupName = $_
+    $Members = Get-Members -Domain $Domain -RepGroupName $RepGroupName
+    Get-Folders -Domain $Domain -RepGroupName $RepGroupName | Foreach {
+        Foreach ($Member in $Members){
+            Que-Job -Member $Member -RepGroupName $RepGroupName -ReplicatedFolder $_
+            }
+    }
+}#$NetRepGroupNames | Foreach {
+}#Foreach ($Domain in $Domains){
+while($val -ne 10){
+    $val++
+    Get-Job | Wait-Job -Timeout 5400 | Out-Null
+    Gather-Jobs
+}
+Get-Job | Wait-Job -Timeout 5400 | Out-Null
+Process-Jobs
+$Output | Sort-Object 'Rep Group','Replicated Folder','Sending Member' | Export-Csv $LogFile -NoTypeInformation
+If ($EmailBody -notlike ''){Send-MailMessage -To $To -Subject $Subject -From $From -BodyAsHtml -Body ($EmailBody | ConvertTo-Html -head $Style -body "<H2>DFSr Backlog Counts Greater than $BacklogThreshold.</H2>Jobs Ran = $($Global:JobCount - $Global:ReRanJobCount), Jobs Processed = $Global:ProcessedJobCount <br><br>" -PostContent "<br><br>Detailed Log - $($LogFile.Replace('C:\',"\\$(get-content env:computername)\$($LogFile.substring(0,1))$\"))<br><br>Total Elapsed Time: $($elapsed.Elapsed.ToString())" | Out-String) -SmtpServer $MailServer}
+Else{Send-MailMessage -To $To -Subject $Subject -From $From -BodyAsHtml -Body (ConvertTo-Html -body "<H2>DFSr Backlog Counts Greater than $BacklogThreshold.</H2>Jobs Ran = $($Global:JobCount - $Global:ReRanJobCount), Jobs Processed = $Global:ProcessedJobCount<br><br><font size=6 color=Green>Nothing to Report</font>" -PostContent "<br><br>Detailed Log - $($LogFile.Replace('C:\',"\\$(get-content env:computername)\$($LogFile.substring(0,1))$\"))<br><br>Total Elapsed Time: $($elapsed.Elapsed.ToString())" | Out-String) -SmtpServer $MailServer}
---
+++ Backlog.ps1
@@ -0,0 +1,121 @@
+﻿$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+[string]$LogFile = "$ScriptPath\Logs\$((get-date).ToString('yyyy-MM-dd_hh-mm')).txt"
+If (-Not(Test-Path -Path "$ScriptPath\Logs")){New-Item -ItemType directory -path "$ScriptPath\Logs" -Force | Out-Null}
+    Get-ChildItem -Path "$ScriptPath\Logs" | Where {$_.PSisContainer -eq $false -and $_.LastWriteTime -lt (Get-date).AddDays(-5)} | Remove-Item -Force
+    New-Item -ItemType file -path $LogFile -Force | Out-Null
+#-----------------------------------------------------------------------
+$To = 'Email@SomeWhere.com'
+$From = 'Email@SomeWhere.com'
+$Subject = "DFSr BackLog Count $((get-date).ToString('MM/dd/yyyy'))"
+$MailServer = 'Mail.SomeWhere.com'
+#-----------------------------------------------------------------------
+$a = "<style>"
+$a = $a + "TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}"
+$a = $a + "TH{border-width: 1px;padding: 0px;border-style: solid;border-color: black;background-color:DarkGray;text-align:center}"
+$a = $a + "TD{border-width: 1px;padding: 0px;border-style: solid;border-color: black;text-align:center}"
+$a = $a + "TR:nth-child(odd) {background: #E6E6E6}"
+$a = $a + "</style>"
+#-----------------------------------------------------------------------
+$Domains = 'Domain1','Domain2'
+$Global:OutPut = @()
+Foreach ($Domain in $Domains){
+Out-File -Append $LogFile -InputObject "$Domain" -Force
+$RepGroupCommand = "dfsradmin rg list /Domain:'" + $Domain + "' /attr:rgname"
+$NetRepGroupNames = Invoke-Expression -Command $RepGroupCommand | Where {-not($_ -like 'RgName' -or $_ -like "" -or $_ -like 'Domain System Volume' -or $_ -like 'Command completed successfully.')}
+If($LASTEXITCODE -ne 0 -or $LASTEXITCODE -eq $Null){Out-File -Append $LogFile -InputObject "**ERROR** Unable to retrieve a list of Replication Groups from $Domain **ERROR**`r`n`tCommand - dfsradmin rg list /Domain:$Domain /attr:rgname" -Force
+    #Write-Warning 'Domain - ERROR'
+    $Data = New-Object psobject
+    $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReplication Group&nbsp' -Value 'Could not retrieve'
+    $Data | Add-Member -MemberType "noteproperty" -Name '&nbspFolder Name&nbsp' -Value 'replication'
+    $Data | Add-Member -MemberType "noteproperty" -Name '&nbspSending Member&nbsp' -Value ' groups from'
+    $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReceiving Member&nbsp' -Value $Domain
+    $Data | Add-Member -MemberType "noteproperty" -Name '&nbspBack Log Count&nbsp' -Value '**ERROR**'
+    $Global:OutPut += $Data
+    $LASTEXITCODE = 0
+    }# End of $LastExitCode 'Domain'
+    Foreach ($NetRepGroupName in $NetRepGroupNames){
+        Out-File -Append $LogFile -InputObject "`t$NetRepGroupName -  $(Get-date -Format hh:mm:ss)" -Force
+        $FolderListCommand = "DfsrAdmin RF List /Domain:'" + $Domain + "' /RgName:'" + $NetRepGroupName + "' /attr:RfName"
+        $FolderList = Invoke-Expression -Command $FolderListCommand | Where {-not($_ -like 'RfName' -or $_ -like "" -or $_ -like 'Command completed successfully.')}
+        If($LASTEXITCODE -ne 0){Out-File -Append $LogFile -InputObject "**ERROR** Unable to retrieve a list of Replication Folders for $NetRepGroupName **ERROR**`r`n`tCommand - DfsrAdmin RF List /Domain:$Domain /RgName:$NetRepGroupName /attr:RfName" -Force
+            #Write-Warning 'Folder - ERROR'
+            $Data = New-Object psobject
+            $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReplication Group&nbsp' -Value 'Could not retrieve'
+            $Data | Add-Member -MemberType "noteproperty" -Name '&nbspFolder Name&nbsp' -Value 'replication'
+            $Data | Add-Member -MemberType "noteproperty" -Name '&nbspSending Member&nbsp' -Value ' folders from'
+            $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReceiving Member&nbsp' -Value $NetRepGroupName
+            $Data | Add-Member -MemberType "noteproperty" -Name '&nbspBack Log Count&nbsp' -Value '**ERROR**'
+            $Global:OutPut += $Data
+            $LASTEXITCODE = 0
+            }# End of $LastExitCode 'Folder'
+        Foreach ($Folder in $FolderList){
+            Out-File -Append $LogFile -InputObject "`t`t$Folder" -Force
+            $MembersCommand = "DfsrAdmin Conn list /Domain:'" + $Domain + "' /RgName:'" + $NetRepGroupName + "' '/attr:SendMem,RecvMem'"
+            $Members = Invoke-Expression -Command $MembersCommand | Where {-not($_ -like 'Sendmem*RecvMem' -or $_ -like "" -or $_ -like 'Command completed successfully.')}
+            If($LASTEXITCODE -ne 0){Out-File -Append $LogFile -InputObject "**ERROR** Unable to retrieve a list of Servers for $NetRepGroupName **ERROR**`r`n`tCommand - DfsrAdmin Conn list /Domain:$Domain /RgName:$NetRepGroupName '/attr:SendMem,RecvMem'" -Force
+                #Write-Warning 'Members - ERROR'
+                $Data = New-Object psobject
+                $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReplication Group&nbsp' -Value 'Could not retrieve'
+                $Data | Add-Member -MemberType "noteproperty" -Name '&nbspFolder Name&nbsp' -Value 'replication'
+                $Data | Add-Member -MemberType "noteproperty" -Name '&nbspSending Member&nbsp' -Value ' members from'
+                $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReceiving Member&nbsp' -Value $NetRepGroupName
+                $Data | Add-Member -MemberType "noteproperty" -Name '&nbspBack Log Count&nbsp' -Value '**ERROR**'
+                $Global:OutPut += $Data
+                $LASTEXITCODE = 0
+                }# End of $LastExitCode 'Members'
+            Foreach ($Member in $Members){
+                $SplitMember = $Member -Split ' {2,}'
+                Out-File -Append $LogFile -InputObject "`t`t`t$($SplitMember[0]) --> $($SplitMember[1])" -Force
+                $BacklogCommand = "Dfsrdiag backlog /Domain:'" + $Domain + "' /ReceivingMember:'" + $($SplitMember[1]) + "' /SendingMember:'" + $($SplitMember[0]) + "' /RgName:'" + $NetRepGroupName + "' /RFName:'" + $Folder + "'"
+                $BackLogCount = Invoke-Expression -Command $BacklogCommand | Where {$_ -like '*Backlog File Count:*'}
+                If($LASTEXITCODE -ne 0){
+                    $RetryCount = 1
+                    Do{
+                        Sleep -Seconds 5
+                        Out-File -Append $LogFile -InputObject "`t`t`tError Retrying - $RetryCount - $(Get-date -Format hh:mm:ss)"
+                        $LASTEXITCODE = 0
+                        $BackLogCount = Invoke-Expression -Command $BacklogCommand | Where {$_ -like '*Backlog File Count:*'}
+                        $RetryCount++
+                    }# End of While
+                    Until($RetryCount -eq 11 -or $LASTEXITCODE -eq 0)
+                }# End of If LastExitCode
+                        If($LASTEXITCODE -ne 0){
+                            Out-File -Append $LogFile -InputObject "**ERROR** Unable to retrieve a Backlog Count, SendingMember:$($SplitMember[0]), ReceivingMember:$($SplitMember[1]) **ERROR**`r`n`tCommand - $BacklogCommand" -Force
+                            #Write-Warning 'Backlog - ERROR'
+                            $Data = New-Object psobject
+                            $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReplication Group&nbsp' -Value $NetRepGroupName
+                            $Data | Add-Member -MemberType "noteproperty" -Name '&nbspFolder Name&nbsp' -Value $Folder
+                            $Data | Add-Member -MemberType "noteproperty" -Name '&nbspSending Member&nbsp' -Value $($SplitMember[0])
+                            $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReceiving Member&nbsp' -Value $($SplitMember[1])
+                            $Data | Add-Member -MemberType "noteproperty" -Name '&nbspBack Log Count&nbsp' -Value '**ERROR**'
+                            $Global:OutPut += $Data
+                        }# End of $LastExitCode 'Backlog'
+                        Foreach ($BackLog in $BackLogCount){
+                            $SplitBackLog = $BackLog -split ' {1,}'
+                            $Array = Get-Content $LogFile
+                            $Array[-1] = $Array[-1] += " - $($SplitBackLog[-1])"
+                            Out-File $LogFile -InputObject $Array
+                            Out-File -Append $LogFile -InputObject "`t`t`tCommand - $BacklogCommand" -Force
+                            If ([int]$SplitBackLog[-1] -gt 75){
+                                $Data = New-Object psobject
+                                $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReplication Group&nbsp' -Value $NetRepgroupName
+                                $Data | Add-Member -MemberType "noteproperty" -Name '&nbspFolder Name&nbsp' -Value $Folder
+                                $Data | Add-Member -MemberType "noteproperty" -Name '&nbspSending Member&nbsp' -Value $($SplitMember[0])
+                                $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReceiving Member&nbsp' -Value $($SplitMember[1])
+                                $Data | Add-Member -MemberType "noteproperty" -Name '&nbspBack Log Count&nbsp' -Value $($SplitBackLog[-1])
+                                $Global:OutPut += $Data
+                            }# End of If Backlog
+                        }# End of Foreach Backlog
+            }# End of Foreach Member
+        }# End of Foreach Folder
+    }# End fo Foreach Rep Group
+}# End of Foreach Domain
+If ($Global:OutPut.Count -eq 0){
+    $Data = New-Object psobject
+    $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReplication Group&nbsp' -Value 'Nothing'
+    $Data | Add-Member -MemberType "noteproperty" -Name '&nbspFolder Name&nbsp' -Value 'to'
+    $Data | Add-Member -MemberType "noteproperty" -Name '&nbspSending Member&nbsp' -Value 'report'
+    $Data | Add-Member -MemberType "noteproperty" -Name '&nbspReceiving Member&nbsp' -Value ''
+    $Data | Add-Member -MemberType "noteproperty" -Name '&nbspBack Log Count&nbsp' -Value ':-)'
+    $Global:OutPut += $Data}
+Send-MailMessage -to $To -from $From -Subject $Subject -BodyAsHtml -Body ($Global:OutPut | ConvertTo-Html -head $a -body "<H2>DFSr Backlog Count - Greater than 75 files</H2>" -PostContent "<br><br>Detailed Log - $($LogFile.Replace('C:\',"\\$(get-content env:computername)\$($LogFile.substring(0,1))$\"))" | Out-String) -SmtpServer $MailServer
